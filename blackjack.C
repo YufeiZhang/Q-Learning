075d7944b6c5908356d1024a60a9018f5040ec80
Yufei Zhang <yufeiGitHub@gmail.com>
2016-01-20 22:14:39 -0700

    add some testing functions for fun

---
+++ blackjack.C
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Qlearning.H"
+#include "blackjack.H"
+using namespace std;
+
+int main() {
+    cout << "The sum of 3 and 4 is " << testing(3, 4) << endl;
+    cout << "3 times 4 is " << times(3, 4) << endl;
+    return 0;
+}
